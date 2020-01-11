f9a4c77144286df01bd0c38f5ed5475e16415725
hxq <1606117871@qq.com>
2020-01-11 16:28:06 +0800

    第一次提交20200111

---
+++ src/test/java/com/hxq/forum/LearningForumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hxq.forum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningForumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
